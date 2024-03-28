f6c7239b85c4166f75972dd00e3f1e9cba5d2abc
Natalia605207 <149045254+Natalia605207@users.noreply.github.com>
2024-03-28 07:51:51 +0300

    Update CheckoutForm.js

--- src/Stripe/CheckoutForm.js
+++ src/Stripe/CheckoutForm.js
@@ -21,7 +21,7 @@ export const CheckoutForm = () => {
       try {
         const { id } = paymentMethod;
         const response = await axios.post(
-          "https://stripe-backend-okyz.onrender.com",
+          "http://localhost:8080/stripe/charge",
           {
             amount: 999,
             id: id,
